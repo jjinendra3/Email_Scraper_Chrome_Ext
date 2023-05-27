188bffc6e09b9a3a31cdf46e88748f386933752a
Jinendra Jain <117433354+jjinendra3@users.noreply.github.com>
2023-05-27 10:48:48 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-fetch('https://icanhazdadjoke.com/slack').then(data=>data.json()).then(jokeData=>{
-    const joketext=jokeData.attachments[0].text;
-    const jokeele=document.getElementById('content');
-    jokeele.innerText=joketext;
-})
